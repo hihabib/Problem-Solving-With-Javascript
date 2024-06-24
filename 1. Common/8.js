b50cf5e908981a3901d2774b7b4e2032710246c2
Habibul Islam <habibulislam6862@gmail.com>
2024-06-24 16:56:33 +0600

    problem 8 is solved with function

--- 1. Common/8.js
+++ 1. Common/8.js
@@ -1,12 +1,16 @@
-const numbers = [3,50,5,6,7,8,9,20, 3,4,65,2,4];
-
-let secondLargestNumber = 0;
-let largestNumber = numbers[0];
-for(let i = 0; i < numbers.length; i++){
-    if(numbers[i] > largestNumber){
-        secondLargestNumber = largestNumber;
-        largestNumber = numbers[i];
+// Write a function that takes an array of numbers and returns the second largest number in the array.
+function getSecondLargestNumber(numbers) {
+    let secondLargestNumber = 0;
+    let largestNumber = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] > largestNumber){
+            secondLargestNumber = largestNumber;
+            largestNumber = numbers[i];
+        }
     }
-}
 
-console.log(secondLargestNumber)
+   return (secondLargestNumber);
+}
+const nums = [3,50,5,6,7,8,9,20, 3,4,65,2,4];
+console.log(getSecondLargestNumber(nums));
+
