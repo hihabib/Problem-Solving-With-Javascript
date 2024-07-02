2ca66f14931a395c29369916b49aeb0930d1b20f
Habibul Islam <habibulislam6862@gmail.com>
2024-07-02 18:09:45 +0600

    Problem 14 is solved

---
+++ 1. Common/14.js
@@ -0,0 +1,12 @@
+// Write a function that takes a number and returns true if the number is a palindrome (reads the same forwards and backwards) and false otherwise.
+function isPalindromeNumber(num){
+    const strOfNum = num + '';
+    let reversedNum = "";
+    for (let i = strOfNum.length - 1 ; i >= 0 ; i--) {
+        reversedNum += strOfNum[i];
+    }
+    return (strOfNum === reversedNum)
+}
+
+
+console.log(isPalindromeNumber(66))
