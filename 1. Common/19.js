634c862f0907b46623bdf4f8b7652ba51976c914
Habibul Islam <habibulislam6862@gmail.com>
2024-07-13 17:41:03 +0600

    19 is solved

---
+++ 1. Common/19.js
@@ -0,0 +1,15 @@
+// Write a function that takes a number and returns true if it is an Armstrong number and false otherwise.
+
+function isArmstrong(num) {
+    num = num + "";
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+        sum += num[i] ** 3;
+    }
+    return num === sum + "";
+}
+
+
+const result = isArmstrong(154);
+
+console.log(result)
